90ae23925533875e5ade0aa0a984ce490c75c32c
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-10-18 16:18:30 +0100

    added cors config

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const mongoose = require('./config/db')
 
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
+app.use(cors())
 
 const authRoutes = require('./routes/authRoutes')
 app.use('/user', authRoutes)
